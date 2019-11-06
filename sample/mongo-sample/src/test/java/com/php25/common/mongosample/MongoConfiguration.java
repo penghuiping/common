25674f04bb10c592c7347f13a82cbda5aebfa847
penghuiping <575813261@qq.com>
2019-11-06 13:45:56 +0800

    加入 mongo-sample 模块

---
+++ sample/mongo-sample/src/test/java/com/php25/common/mongosample/MongoConfiguration.java
@@ -0,0 +1,25 @@
+package com.php25.common.mongosample;
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+/**
+ * @author penghuiping
+ * @date 2019/11/6 13:29
+ */
+
+public class MongoConfiguration {
+
+    @Bean
+    public SimpleMongoDbFactory simpleMongoDbFactory() {
+        return new SimpleMongoDbFactory(new MongoClient("localhost", 27017), "test");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(simpleMongoDbFactory());
+    }
+
+}
